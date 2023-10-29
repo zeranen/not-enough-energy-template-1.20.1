c80ff738dda98e8b61c1edeb8ed508ce7e4e4a99
zeranen <slava.tolmachv@mail.ru>
2023-10-29 11:12:37 +0500

    Initial commit

---
+++ src/main/java/net/verzex/notenoughenergy/NotEnoughEnergyClient.java
@@ -0,0 +1,10 @@
+package net.verzex.notenoughenergy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughEnergyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
